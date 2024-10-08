34295064756372d4feab2ef1830a5d3b2cf4fbd8
Grzegorz Górski <grzegorz.gorski@rst.com.pl>
2024-10-08 14:57:59 +0200

    cron added

--- tests/api.test.js
+++ tests/api.test.js
@@ -47,7 +47,6 @@ describe("API tests", () => {
   it("Authorize user", async () => {
     const response = await spec()
       .post(`${baseUrl}/Account/v1/Authorized`)
-
       .withBody({
         userName: user,
         password: password,
@@ -68,7 +67,6 @@ describe("API tests", () => {
     const response = await spec()
       .post(`${baseUrl}/BookStore/v1/Books`)
       .withBearerToken(token)
-
       .withBody({
         userId: userId,
         collectionOfIsbns: [
